ecb177aa1b4403caaf0231b0ce5ae3036398c5ed
CodyAdam <cody.adam@laposte.net>
2021-02-02 18:27:10 +0100

    ADDED javadoc EDIT Privatize function

--- src/library/MonHtml2String.scala
+++ src/library/MonHtml2String.scala
@@ -1,7 +1,12 @@
 package library
 
 object MonHtml2String extends Html2String { // Thomas D
-
+  /**
+   * Produit la chaîne de caractère correspondant à un document Html
+   *
+   * @param h le document Html
+   * @return la chaîne de caractère représentant h
+   */
   def process(h: Html): String = {
     h match {
       case (Tag(balise, attributs, childs)) => "<" + balise + processRecAttributs(attributs) + ">" + "\n" + processRec(childs) + "</" + balise + ">" //gère les balises
@@ -10,7 +15,7 @@ object MonHtml2String extends Html2String { // Thomas D
     }
   }
 
-  def processRec(l: List[Html]): String = {
+  private def processRec(l: List[Html]): String = {
     l match {
       case (Tag(balise, attributs, childs) :: d) => "<" + balise + processRecAttributs(attributs) + ">" + "\n" + processRec(childs) + "</" + balise + ">" + "\n" + processRec(d)
       case Text(texte) :: reste                  => texte + "\n" + processRec(reste)
@@ -18,7 +23,7 @@ object MonHtml2String extends Html2String { // Thomas D
     }
   }
 
-  def processRecAttributs(l: List[(String, String)]): String = {
+  private def processRecAttributs(l: List[(String, String)]): String = {
     l match {
       case (att1, att2) :: Nil   => " " + att1 + "=\"" + att2 + "\""
       case (att1, att2) :: reste => " " + att1 + "=\"" + att2 + "\";" + processRecAttributs(reste)
