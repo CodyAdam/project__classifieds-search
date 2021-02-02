bf9f9584035a7fde4608d555f092b38bd14f6ca8
CodyAdam <cody.adam@laposte.net>
2021-02-02 18:26:41 +0100

    Format and clean code

--- src/library/MonFiltrageURLs.scala
+++ src/library/MonFiltrageURLs.scala
@@ -10,10 +10,8 @@ object MonFiltrageURLs extends FiltrageURLs { // par Arthur et Mael
    * @param h le document Html
    * @return la liste des URLs d'annonces contenues dans h
    */
-
   def filtreAnnonce(h: Html): List[String] = {
-    var lH = List(h)
-    filtreAnnonceRec(lH)
+    filtreAnnonceRec(List(h))
   }
 
   private def filtreAnnonceRec(h: List[Html]): List[String] = {
@@ -24,33 +22,22 @@ object MonFiltrageURLs extends FiltrageURLs { // par Arthur et Mael
         case Tag(a, l, child) =>
           if (a.equals("a")) {
             val link = linkIsValid(l);
-            if(link != "") List(link) ++ filtreAnnonceRec(child) ++ filtreAnnonceRec(l2)
+            if (link != "") List(link) ++ filtreAnnonceRec(child) ++ filtreAnnonceRec(l2)
             else filtreAnnonceRec(child) ++ filtreAnnonceRec(l2)
-          }
-          else
+          } else
             filtreAnnonceRec(child) ++ filtreAnnonceRec(l2)
       }
     }
   }
 
-  /*private def baliseListeMatch(l: List[(String, String)]): String = {
-    l match {
-      case Nil => ""
-      case (a, b) :: l2 =>
-        if (a == "href") b
-        else baliseListeMatch(l2)
-    }
-  }*/
-  
-  private def linkIsValid(l: List[(String, String)]) : String = {
+  private def linkIsValid(l: List[(String, String)]): String = {
     var link = "";
     var isAProduct = false;
-    for ((attributeName,attributeValue) <- l){
-      if(attributeName == "class" && attributeValue == "clad__ad_link") isAProduct = true;
-      else if(attributeName == "href" && attributeValue.contains("http")) link = attributeValue;
+    for ((attributeName, attributeValue) <- l) {
+      if (attributeName == "class" && attributeValue == "clad__ad_link") isAProduct = true;
+      else if (attributeName == "href" && attributeValue.contains("http")) link = attributeValue;
     }
-    if(isAProduct) return link;
+    if (isAProduct) return link;
     return "";
   }
-
 }
