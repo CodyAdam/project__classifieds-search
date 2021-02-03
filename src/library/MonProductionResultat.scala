b87f8cef556610bf71c9dcaa945638728c44c508
Thomega35 <61747645+Thomega35@users.noreply.github.com>
2021-02-03 02:51:23 +0100

    Ajout transition

--- src/library/MonProductionResultat.scala
+++ src/library/MonProductionResultat.scala
@@ -1,8 +1,7 @@
 package library
 
-object MonProductionResultat extends ProductionResultat { // William et Thomas D
-  final val css: String = "a{text-decoration:none;font-size:18px;margin:4px;font-family:'Roboto', sans-serif;font-weight:300;color:white;} a:hover{transition:all 0.05s;font-size:19px;color:rgb(122, 71, 243);} h1{margin:30px;font-size:40px;font-family:'Roboto', sans-serif;font-weight:900;}.links{display:flex;justify-content:center;align-items:center;flex-direction:column;margin:0;}.container{height:100%;width:100%;display:flex;justify-content:center;align-items:center;flex-direction:column;margin:0;}body{color:white;background-color:rgb(26, 25, 27);}"
-
+object MonProductionResultat extends ProductionResultat { // Thomas D et Cody
+final val css: String = "a{text-decoration:none;font-size:18px;margin:4px;font-family:'Roboto', sans-serif;font-weight:300;color:white;transition: transform ease .5s;} \na:hover{transform: scale(1.2);font-size:25px;color:rgb(122, 71, 243);} \nh1{margin:30px;font-size:40px;font-family:'Roboto', sans-serif;font-weight:900;}\n.links{display:flex;justify-content:center;align-items:center;flex-direction:column;margin:0;}\n.container{height:100%;width:100%;display:flex;justify-content:center;align-items:center;flex-direction:column;margin:0;}\nbody{color:white;background-color:rgb(26, 25, 27);}"
   /**
    * A partir d'une liste de couples (titre,URL), produit un document Html, qui
    * liste les solutions sous la forme de liens cliquables
@@ -22,14 +21,14 @@ object MonProductionResultat extends ProductionResultat { // William et Thomas D
 
         Tag("body", List(), List(
           Tag("div", List(("class", "container")), List(
-            Tag("h1", List(), List(Text("Voici les résultats de vos recherches"))),
+            Tag("h1", List(), List(Text("Voici les résultats de votre recherche :"))),
             Tag("div", List(("class", "links")), resultat2htmlRec(l))))))))
   }
 
   private def resultat2htmlRec(l: List[(String, String)]): List[Tag] = {
     l match {
       case (title, link) :: rest =>
-        Tag("p",List(),List(Tag("a", List(("href", link)), List(Text(title))))) :: resultat2htmlRec(rest)
+       Tag("a", List(("href", link)), List(Text(title))) :: resultat2htmlRec(rest)
       case _ => List()
     }
   }
