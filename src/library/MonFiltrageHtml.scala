1c5d19ecf2cd759c4f86638aafddcbc4a9e40540
Vanyx <43043565+Vanyx@users.noreply.github.com>
2021-02-03 08:48:39 +0100

    Update MonFiltrageHtml.scala
    Ajout de commentaires et rennomage des variables

--- src/library/MonFiltrageHtml.scala
+++ src/library/MonFiltrageHtml.scala
@@ -1,40 +1,54 @@
 package library
 
 object MonFiltrageHtml extends FiltrageHtml { // Par Thomas B
-  /**
-   * A partir d'un document Html h et d'une requête e, dit si le document
-   * satisfait l'expression e
-   *
-   * @param h le document Html
-   * @param e l'expression
-   * @return true si le document satisfait l'expression e
-   */
-  def filtreHtml(h: Html, e: Expression): Boolean = {
-    e match {
-      case Word(w)   => annexe(h, w)
-      case And(a, b) => filtreHtml(h, a) && filtreHtml(h, b)
-      case Or(a, b)  => filtreHtml(h, a) || filtreHtml(h, b)
-    }
-  }
+	/**
+	 * A partir d'un document Html h et d'une requête e, dit si le document
+	 * satisfait l'expression e
+	 *
+	 * @param h le document Html
+	 * @param e l'expression
+	 * @return true si le document satisfait l'expression e
+	 */
+	def filtreHtml(h: Html, e: Expression): Boolean = {
+			e match {
+			case Word(mot)   => annexe(h, mot)
+			case And(expression1, expression2) => filtreHtml(h, expression1) && filtreHtml(h, expression2)
+			case Or(expression1, expression2)  => filtreHtml(h, expression1) || filtreHtml(h, expression2)
+			}
+	}
 
-  private def annexe(h: Html, e: String): Boolean = {
-    h match {
-      case Text(a) => a.contains(e)
-      case Tag(_, _, a) => {
-        a match {
-          case first :: Nil   => annexe(first, e)
-          case first :: reste => annexe(first, e) || annexe2(reste, e)
-          case Nil            => false
-        }
-      }
-    }
-  }
+	/**
+	 * A partir d'un document Html h et d'un String e, dit si le document contient e
+	 * 
+	 * @param h le document html
+	 * @param e le String
+	 * @return true si le document contient e
+	 */
+	def annexe(h: Html, e: String): Boolean = {
+			h match {
+			case Text(texte) => texte.contains(e)
+			case Tag(_, _, htmlList) => {
+				htmlList match {
+				case first :: Nil   => annexe(first, e)
+				case first :: reste => annexe(first, e) || annexe2(reste, e)
+				case Nil            => false
+				}
+			}
+			}
+	}
 
-  private def annexe2(h: List[Html], e: String): Boolean = {
-    h match {
-      case first :: Nil   => annexe(first, e)
-      case first :: reste => annexe(first, e) || annexe2(reste, e)
-      case Nil            => false
-    }
-  }
+	/**
+	 * A partir d'une liste de document Html h et d'un String e, dit si les document contienent e
+	 * 
+	 * @param h la liste de document html
+	 * @param e le String
+	 * @return true si e est contenu dans la liste des documents html
+	 */
+	def annexe2(h: List[Html], e: String): Boolean = {
+			h match {
+			case first :: Nil   => annexe(first, e)
+			case first :: reste => annexe(first, e) || annexe2(reste, e)
+			case Nil            => false
+			}
+	}
 }
